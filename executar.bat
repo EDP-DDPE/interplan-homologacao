e9f1298a5f060c752161c644bcae87debba5dc0b
Giovana da Penha Rocha <88893667+giovanapr@users.noreply.github.com>
2026-08-24 10:59:47 -0300

    Update executar.bat

--- executar.bat
+++ executar.bat
@@ -1,19 +1,11 @@
 @echo off
 
-set REPO=C:\Ferramentas\Interplan-Homologacao
-
-if not exist "%REPO%" (
-  echo Repositorio nao encontrado.
-  pause
-  exit
-)
-
-cd /d "%REPO%"
-
-git pull
-
+echo Instalando dependencias...
 pip install -r requirements.txt
 
-python main.py "%CD%"
+echo Executando comparacao...
+python main.py
 
+echo.
+echo Processo concluido.
 pause
